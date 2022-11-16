8172ff042743f3addfaad5b5cd58950e7b908515
Cybereasy <118338725+Mrcybereasy@users.noreply.github.com>
2022-11-16 13:08:44 +0530

    Add files via upload

---
+++ y.sh
@@ -0,0 +1,107 @@
+#bash/bin
+clear
+figlet Decrypter
+sleep 0.6
+echo $'\e[1;36m'[*] Made by - CyberEasy [*]
+sleep 1.3
+echo "____________________________________________"
+echo $'\e[1;32m'
+cd $HOME
+echo "------->Give the path of File( /sdcard ) : "
+read h
+cd $h
+echo "------->File Name:  "
+read c
+echo "-------> Folder Name : "
+read f
+
+mkdir $f
+cd $f
+cp -r $h/$c $h/$f/
+mv $c f1-$c.html
+cp -r $h/$c $h/$f/
+mv $c f2-$c.txt
+cp -r $h/$c $h/$f/
+mv $c f3-$c.pdf
+cp -r $h/$c $h/$f/
+mv $c f4-$c.zip
+cp -r $h/$c $h/$f/
+mv $c f5-$c.mp3
+cp -r $h/$c $h/$f/
+mv $c f6-$c.mp4
+cp -r $h/$c $h/$f/
+mv $c f7-$c.mkv
+cp -r $h/$c $h/$f/
+mv $c f8-$c.jpg
+cp -r $h/$c $h/$f/
+mv $c f9-$c.png
+cp -r $h/$c $h/$f/
+mv $c f10-$c.jpge
+cp -r $h/$c $h/$f/
+mv $c f11-$c.docx
+cp -r $h/$c $h/$f/
+mv $c f12-$c.xls
+cp -r $h/$c $h/$f/
+mv $c f13-$c.xml
+cp -r $h/$c $h/$f/
+mv $c f14-$c.yml
+cp -r $h/$c $h/$f/
+mv $c f15-$c.exe
+cp -r $h/$c $h/$f/
+mv $c f16-$c.bat
+cp -r $h/$c $h/$f/
+mv $c f17-$c.apk
+cp -r $h/$c $h/$f/
+mv $c f18-$c.wav
+cp -r $h/$c $h/$f/
+mv $c f19-$c.aac
+cp -r $h/$c $h/$f/
+mv $c f20-$c.wma
+cp -r $h/$c $h/$f/
+mv $c f21-$c.ac3
+cp -r $h/$c $h/$f/
+mv $c f22-$c.one
+cp -r $h/$c $h/$f/
+mv $c f23-$c.cmd
+cp -r $h/$c $h/$f/
+mv $c f24-$c.with
+cp -r $h/$c $h/$f/
+mv $c f25-$c.bmp
+cp -r $h/$c $h/$f/
+mv $c f26-$c.gif
+cp -r $h/$c $h/$f/
+mv $c f27-$c.tiff
+cp -r $h/$c $h/$f/
+mv $c f28-$c.doc
+cp -r $h/$c $h/$f/
+mv $c f29-$c.rtf
+cp -r $h/$c $h/$f/
+mv $c f30-$c.tex
+cp -r $h/$c $h/$f/
+mv $c f31-$c.wpd
+cp -r $h/$c $h/$f/
+mv $c f32-$c.rar
+cp -r $h/$c $h/$f/
+mv $c f33-$c.gz
+cp -r $h/$c $h/$f/
+mv $c f34-$c.arj
+cp -r $h/$c $h/$f/
+mv $c f35-$c.avi
+cp -r $h/$c $h/$f/
+mv $c f36-$c.mpeg
+cp -r $h/$c $h/$f/
+mv $c f37-$c.wmv
+cp -r $h/$c $h/$f/
+mv $c f38-$c.dwg
+cp -r $h/$c $h/$f/
+mv $c f39-$c.sh
+cp -r $h/$c $h/$f/
+mv $c f40-$c.php
+cp -r $h/$c $h/$f/
+mv $c f41-$c.py
+
+
+sleep 2.0
+toilet -f future DONE...
+sleep 1.0
+toilet -f future thanks
